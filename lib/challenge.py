2f466bad7139bfd9e0d3f7e64fe77739841ec8c6
Bernard Lee <bernardlee82@gmail.com>
2017-01-24 08:37:21 -0500

    Add starter code
    Add function definitions with empty bodies in lib/challenge.* files.

---
+++ lib/challenge.py
@@ -0,0 +1,14 @@
+def all():
+    # Your code here.
+
+
+def any():
+    # Your code here.
+
+
+def none():
+    # Your code here.
+
+
+def one():
+    # Your code here.
---
+++ lib/challenge.rb
@@ -0,0 +1,15 @@
+def all
+  # Your code here.
+end
+
+def any
+  # Your code here.
+end
+
+def none
+  # Your code here.
+end
+
+def one
+  # Your code here.
+end
