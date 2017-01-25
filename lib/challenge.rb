63bc310fec68d446f2c2610fa59e5f7f4e60e159
Bernard Lee <bernardlee82@gmail.com>
2017-01-25 09:13:21 -0500

    Add Ruby solutions
    Add a Ruby solution to each challenge.

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,23 +1,19 @@
 # Requirements
 
-# Add your function parameters between the parentheses.
-def any?()
-  # Your function body here.
+def any?(array, &block)
+  array.any?(&block)
 end
 
-# Add your function parameters between the parentheses.
-def none?()
-  # Your function body here.
+def none?(array, &block)
+  array.none?(&block)
 end
 
 # Bonuses
 
-# Add your function parameters between the parentheses.
-def one?()
-  # Your function body here.
+def one?(array, &block)
+  array.one?(&block)
 end
 
-# Add your function parameters between the parentheses.
-def all?()
-  # Your function body here.
+def all?(array, &block)
+  array.all?(&block)
 end
