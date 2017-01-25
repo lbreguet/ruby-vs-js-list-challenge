450aa8e53aefc0a82dae30911be3458d4a00d201
Bernard Lee <bernardlee82@gmail.com>
2017-01-25 15:44:30 -0500

    Add Python solutions
    Add a Python solution to each challenge.

--- lib/challenge.py
+++ lib/challenge.py
@@ -2,26 +2,22 @@
 
 
 # Add your function parameters between the parentheses.
-def any():
-    # Your function body here.
-    return
+def challenge_any(array, predicate):
+    return any(map(predicate, array))
 
 
 # Add your function parameters between the parentheses.
-def none():
-    # Your function body here.
-    return
+def challenge_none(array, predicate):
+    return len(list(filter(lambda e: e, map(predicate, array)))) == 0
 
 # Bonuses
 
 
 # Add your function parameters between the parentheses.
-def one():
-    # Your function body here.
-    return
+def challenge_one(array, predicate):
+    return len(list(filter(lambda e: e, map(predicate, array)))) == 1
 
 
 # Add your function parameters between the parentheses.
-def all():
-    # Your function body here.
-    return
+def challenge_all(array, predicate):
+    return len(list(filter(lambda e: e, map(predicate, array)))) == len(array)
