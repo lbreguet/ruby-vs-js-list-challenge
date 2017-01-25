f1a160caaaf253a62d49443d51d85afeb6437143
Bernard Lee <bernardlee82@gmail.com>
2017-01-25 08:13:43 -0500

    Rearrange the order of functions
    Separate the functions into requirements and bonuses.

--- lib/challenge.py
+++ lib/challenge.py
@@ -1,18 +1,27 @@
-# Add your function parameters in between the parentheses.
-def all():
-    # Your function body here.
+# Requirements
 
 
-# Add your function parameters in between the parentheses.
+# Add your function parameters between the parentheses.
 def any():
     # Your function body here.
+    return
 
 
-# Add your function parameters in between the parentheses.
+# Add your function parameters between the parentheses.
 def none():
     # Your function body here.
+    return
+
+# Bonuses
 
 
-# Add your function parameters in between the parentheses.
+# Add your function parameters between the parentheses.
 def one():
     # Your function body here.
+    return
+
+
+# Add your function parameters between the parentheses.
+def all():
+    # Your function body here.
+    return
--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,19 +1,23 @@
-# Add your function parameters in between the parentheses.
-def all?()
-  # Your function body here.
-end
+# Requirements
 
-# Add your function parameters in between the parentheses.
+# Add your function parameters between the parentheses.
 def any?()
   # Your function body here.
 end
 
-# Add your function parameters in between the parentheses.
+# Add your function parameters between the parentheses.
 def none?()
   # Your function body here.
 end
 
-# Add your function parameters in between the parentheses.
+# Bonuses
+
+# Add your function parameters between the parentheses.
 def one?()
+  # Your function body here.
+end
+
+# Add your function parameters between the parentheses.
+def all?()
   # Your function body here.
 end
